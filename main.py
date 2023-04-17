77ed75589194285dc8547a4d8804d3399ee51969
Evan SooHoo <evan.soohoo@gmail.com>
2023-04-16 21:32:19 -0700

    initialized websocket logic

--- main.py
+++ main.py
@@ -24,8 +24,25 @@ def transcribe(audio):
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages);
     return response;
 
+async def pushToTalkButtonPushed(request):
+    #https://docs.aiohttp.org/en/stable/web_quickstart.html#aiohttp-web-websockets
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    async for audio in transcribe():
+        await ws.send_bytes(audio)
+
+    return ws
+
+app = web.Application()
+app.add_routes([
+    web.get('/', pushToTalkButtonPushed)
+])
+
+if __name__ == '__main__':
+    web.run_app(app, port=8099)
 #def initWeb():
 #    app = web.Application() #https://docs.aiohttp.org/en/stable/web_quickstart.html
 #    app.router.add.get("/", )
 
-
+#ui = gradio.Interface(fn=transcribe, inputs=gradio.Audio(source="microphone", type="filepath"), outputs="text").launch()
+#ui.launch()
